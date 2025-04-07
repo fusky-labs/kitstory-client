15628c946d39a7a7bfd705c6747985f3a9bb54be
Kerby Keith Aquino <hello@kurojifusky.com>
2025-04-07 21:06:28 +0800

    remove ded submodules and code

--- packages/config/tw-config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss"
-import kuroTwConfig from "@kuro-utils/tailwind"
-
-const config: Config = {
-  content: [],
-  presets: [kuroTwConfig]
-}
-
-export default config
