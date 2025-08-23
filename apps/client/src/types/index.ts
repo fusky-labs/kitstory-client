5a163966c4d4b2252004b0d5c418493901f44482
Kuro <hello@kurojifusky.com>
2025-08-23 10:10:30 +0800

    more biome stuff

--- apps/client/src/types/index.ts
+++ apps/client/src/types/index.ts
@@ -1,17 +1,17 @@
 type BlockType = "dialogue" | "meta"
 
-interface DefineBlock<D extends Record<string, any>> {
-  uuid: ReturnType<typeof crypto.randomUUID>
-  type: BlockType
-  data: D
+interface DefineBlock<D extends Record<string, unknown>> {
+	uuid: ReturnType<typeof crypto.randomUUID>
+	type: BlockType
+	data: D
 }
 
 export interface CharacterItem {
-  name: string
-  description?: string
+	name: string
+	description?: string
 }
 
 export type PlotBlock = DefineBlock<{
-  character?: string | CharacterItem
-  textContent?: string
+	character?: string | CharacterItem
+	textContent?: string
 }>
