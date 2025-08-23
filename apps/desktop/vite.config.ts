03cd916f94f22343dbd2f32676d91343f0ae76f2
Kuro <hello@kurojifusky.com>
2025-08-23 18:44:32 +0800

    sharing css

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
-import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
-// @ts-expect-error: tailwindcss/vite plugin works, but its types seem incompatible for the time being
 export default defineConfig(async () => ({
   plugins: [
     tailwindcss(),
